40038c19a1bcf7436367b7dbdff4527ecd4a9d66
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-06-05 01:41:16 +0300

    feat:will add models

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,3 +1,44 @@
+from tkinter import CASCADE
+from xmlrpc.client import DateTime
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.utils.timezone import now
 # Create your models here.
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'postImages', blank=False)
+    caption =models.TextField(max_length=500)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField()
+    slug = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.author
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    dateTime = models.ForeignKey('self', on_delete=models.CASCADE)
+    posted_at = models.DateTimeField(default=now)
+    
+    def __str__(self):
+        return self.user
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to = 'profile_pics', blank=True)
+    bio = models.TextField()
+    
+    def __str__(self):
+        return self.user
+    
+class Follow(models.Model):
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
--- instagramcore/settings.py
+++ instagramcore/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'instagramcore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER':'moringa',
+        'PASSWORD':'268231'
     }
 }
 
